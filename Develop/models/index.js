ea5c6cecee716926e1ccd684c6468a6eb94cfec0
ktp1451 <76793752+ktp1451@users.noreply.github.com>
2021-05-06 16:26:43 -0500

    updated code

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,15 +5,38 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Buyer, {
-  foreignKey: 'product_name',
+// Product.belongsTo(Buyer, {
+//   foreignKey: 'product_name',
+// });
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
 });
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  foreignKey: 'product_id'
+  //Define an allias for when data is retrieved
+  //as: 'lookup_product'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
